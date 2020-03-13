507a3824a5ea3a247406d0935c7acbbb30b1b2c3
mgkOV <myagkov.mww@gmail.com>
2020-03-14 01:14:23 +0300

    add settings route path

--- routes/settings.js
+++ routes/settings.js
@@ -1,33 +1,53 @@
 const express = require("express");
 const axios = require("axios");
 const config = require("config");
+const https = require("https");
 
 const router = express.Router();
 
-const jwt = config.get("jwt");
+const JWT = config.get("jwt");
+const URL = "https://hw.shri.yandex/api/conf";
+
+const agent = new https.Agent({
+  rejectUnauthorized: false
+});
 
 router.get("/", async (req, res) => {
+  const response = await axios.get(URL, {
+    headers: { Authorization: `Bearer ${JWT}` },
+    httpsAgent: agent
+  });
+
+  const { repoName, buildCommand, mainBranch, period } = response.data.data;
+
   const settings = {
-    repoName: "string",
-    buildCommand: "string",
-    mainBranch: "string",
-    period: 0
+    repoName,
+    buildCommand,
+    mainBranch,
+    period
   };
 
-  res.json({ settings });
+  res.json(settings);
 });
 
 router.post("/", async (req, res) => {
-  // const { repoName, buildCommand, mainBranch, period } = req.body;
+  const { repoName, buildCommand, mainBranch, period } = req.body;
 
   const settings = {
-    repoName: "string",
-    buildCommand: "string",
-    mainBranch: "string",
-    period: 0
+    repoName,
+    buildCommand,
+    mainBranch,
+    period
   };
 
-  res.sendStatus(201);
+  console.log(settings);
+
+  const response = await axios.post(URL, settings, {
+    headers: { Authorization: `Bearer ${JWT}` },
+    httpsAgent: agent
+  });
+
+  res.sendStatus(response.status);
 });
 
 module.exports = router;
