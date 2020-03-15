84abd011cc147771483777ca02cc13abb5c65d90
mgkOV <myagkov.mww@gmail.com>
2020-03-16 02:03:29 +0300

    add build request route

--- routes/builds.js
+++ routes/builds.js
@@ -6,7 +6,7 @@ const https = require("https");
 const router = express.Router();
 
 const JWT = config.get("jwt");
-const URL = "https://hw.shri.yandex/api/build";
+const URL = "https://hw.shri.yandex/api";
 
 const agent = new https.Agent({
   rejectUnauthorized: false
@@ -15,7 +15,7 @@ const agent = new https.Agent({
 // получение списка сборок
 router.get("/", async (req, res) => {
   const { offset = 0, limit = 25 } = req.query;
-  const response = await axios.get(`${URL}/list?offset=${offset}&limit=${limit}`, {
+  const response = await axios.get(`${URL}/build/list?offset=${offset}&limit=${limit}`, {
     headers: { Authorization: `Bearer ${JWT}` },
     httpsAgent: agent
   });
@@ -27,7 +27,7 @@ router.get("/", async (req, res) => {
 router.get("/:buildId", async (req, res) => {
   const { buildId } = req.params;
 
-  const response = await axios.get(`${URL}/details?buildId`, {
+  const response = await axios.get(`${URL}/build/details?buildId=${buildId}`, {
     headers: { Authorization: `Bearer ${JWT}` },
     httpsAgent: agent
   });
@@ -39,7 +39,7 @@ router.get("/:buildId", async (req, res) => {
 router.get("/:buildId/logs", async (req, res) => {
   const { buildId } = req.params;
 
-  const response = await axios.get(`${URL}/log?buildId=${buildId}`, {
+  const response = await axios.get(`${URL}/build/log?buildId=${buildId}`, {
     headers: { Authorization: `Bearer ${JWT}` },
     httpsAgent: agent
   });
@@ -50,7 +50,33 @@ router.get("/:buildId/logs", async (req, res) => {
 // добавление сборки в очередь
 router.post("/:commitHash", async (req, res) => {
   const { commitHash } = req.params;
-  res.json({ post: `/builds/${commitHash}` });
+
+  const response = await axios.get(`${URL}/conf`, {
+    headers: { Authorization: `Bearer ${JWT}` },
+    httpsAgent: agent
+  });
+
+  const { repoName, buildCommand, mainBranch, period } = response.data.data;
+
+  const getCommitsResponse = await axios.get(
+    `https://api.github.com/repos${repoName}/commits/${commitHash}`
+  );
+
+  const { sha, commit } = getCommitsResponse.data;
+
+  const commitData = {
+    commitMessage: commit.message,
+    commitHash: sha,
+    branchName: mainBranch,
+    authorName: commit.author.name
+  };
+
+  const status = await axios.post(`${URL}/build/request`, commitData, {
+    headers: { Authorization: `Bearer ${JWT}` },
+    httpsAgent: agent
+  });
+
+  res.sendStatus(status.status);
 });
 
 module.exports = router;
