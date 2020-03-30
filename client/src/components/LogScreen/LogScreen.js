abc849748c4c2b089538bfb40847b8d4d53520bc
mgkOV <myagkov.mww@gmail.com>
2020-03-30 20:42:49 +0300

    add colored log

--- client/src/components/LogScreen/LogScreen.js
+++ client/src/components/LogScreen/LogScreen.js
@@ -1,8 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { cn } from "@bem-react/classname";
-
+import Convert from "ansi-to-html";
 import "./LogScreen.scss";
+
+const convert = new Convert({ fg: "#000", bg: "#000", newline: true });
 
 const propTypes = {
   log: PropTypes.string,
@@ -11,8 +13,9 @@ const propTypes = {
 
 const LogScreen = ({ children, mix, log }) => {
   const logScreenStyles = cn("LogScreen")(null, mix);
+  const logHtml = { __html: convert.toHtml(log) };
 
-  return <div className={logScreenStyles}>{log}</div>;
+  return <div className={logScreenStyles} dangerouslySetInnerHTML={logHtml} />;
 };
 
 LogScreen.propTypes = propTypes;
