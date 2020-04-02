7b2d7853c32a9343a9d9544b691437932f4e7b0d
mgkOV <myagkov.mww@gmail.com>
2020-04-02 13:59:24 +0300

    delete lowcase folder

--- client/src/components/app/index.js
+++
@@ -1,6 +0,0 @@
-import App from "./App";
-
-import "./Footer/App-Footer";
-import "./Section/App-Section";
-
-export default App;
--- client/src/components/header/Logo/Header-Logo.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./Header-Logo.scss";
-
-const HeaderLogo = () => {
-  return <h1 className="Header-Logo">School CI server</h1>;
-};
-
-export default HeaderLogo;
