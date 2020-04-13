940496871519907b50380354393adee452ad66c5
Jaa <mgkov>
2020-04-13 23:35:21 +0300

    add GET build list test

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,10 +1,10 @@
 const app = require('../server');
 const request = require('supertest');
 
-describe('Routes', () => {
+describe('Settings routes', () => {
   test('GET /api/settings should return settings', (done) => {
     request(app)
-      .get('/api/settings/')
+      .get('/api/settings')
       .expect(200)
       .expect({
         id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
@@ -18,7 +18,7 @@ describe('Routes', () => {
 
   test('POST /api/settings should return settings', (done) => {
     request(app)
-      .post('/api/settings/')
+      .post('/api/settings')
       .send({
         repoName: 'string',
         buildCommand: 'string',
@@ -33,6 +33,45 @@ describe('Routes', () => {
         mainBranch: 'string',
         period: 0,
       })
+      .end(done);
+  });
+
+  test('DELETE /api/settings should return status 200', (done) => {
+    request(app).delete('/api/settings').expect(200).end(done);
+  });
+});
+
+describe('Build routes', () => {
+  test('GET /api/builds should return build list', (done) => {
+    request(app)
+      .get('/api/builds')
+      .expect(200)
+      .expect([
+        {
+          id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+          configurationId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+          buildNumber: 0,
+          commitMessage: 'string',
+          commitHash: 'string',
+          branchName: 'string',
+          authorName: 'string',
+          status: 'Waiting',
+          start: '13 апр, 22:32',
+          duration: '-',
+        },
+        {
+          id: '3fa85f64-5717-4562-b3fc-2c963f66afa9',
+          configurationId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+          buildNumber: 0,
+          commitMessage: 'string',
+          commitHash: 'string',
+          branchName: 'string',
+          authorName: 'string',
+          status: 'Waiting',
+          start: '13 апр, 22:32',
+          duration: '-',
+        },
+      ])
       .end(done);
   });
 });
