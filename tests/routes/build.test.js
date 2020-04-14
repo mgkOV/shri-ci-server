d584f964fdef5effe89c8de373f8a4cc5a80d0f3
Jaa <mgkov>
2020-04-14 17:30:43 +0300

    test downloader middleware

--- tests/routes/build.test.js
+++ tests/routes/build.test.js
@@ -3,47 +3,7 @@ jest.mock('../../utils/build-runner');
 const app = require('../../server');
 const request = require('supertest');
 
-describe('Settings routes', () => {
-  test('GET /api/settings should return settings', (done) => {
-    request(app)
-      .get('/api/settings')
-      .expect(200)
-      .expect({
-        id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-        repoName: 'string',
-        buildCommand: 'string',
-        mainBranch: 'string',
-        period: 0,
-      })
-      .end(done);
-  });
-
-  test('POST /api/settings should return settings', (done) => {
-    request(app)
-      .post('/api/settings')
-      .send({
-        repoName: 'string',
-        buildCommand: 'string',
-        mainBranch: 'string',
-        period: 0,
-      })
-      .expect(200)
-      .expect({
-        id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-        repoName: 'string',
-        buildCommand: 'string',
-        mainBranch: 'string',
-        period: 0,
-      })
-      .end(done);
-  });
-
-  test('DELETE /api/settings should return status 200', (done) => {
-    request(app).delete('/api/settings').expect(200).end(done);
-  });
-});
-
-describe('Build routes', () => {
+describe('Builds routes', () => {
   test('GET /api/builds should return build list', (done) => {
     request(app)
       .get('/api/builds')
@@ -124,9 +84,6 @@ describe('Build routes', () => {
   });
 
   test('POST /api/builds/:hash should add build to queue', (done) => {
-    // const addBuildMock = jest.fn();
-
-    // buildRunner.addBuilds = addBuildMock;
     const { addBuilds } = require('../../utils/build-runner');
     addBuilds.mockReset();
 
