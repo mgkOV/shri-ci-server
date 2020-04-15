496f4e5275d014da9b952704c3d534db7d371924
Jaa <mgkov>
2020-04-15 14:40:24 +0300

    add mobile screenshots

--- hermione/app.hermione.js
+++ hermione/app.hermione.js
@@ -14,6 +14,19 @@ describe('App', function () {
     return this.browser.url('/').assertView('Build list screen', 'body');
   });
 
+  it('Build list screenshot must be identical on mobile screen', function () {
+    return this.browser
+      .setViewportSize(
+        {
+          width: 365,
+          height: 865,
+        },
+        false
+      )
+      .url('/')
+      .assertView('Build list screen', 'body', { allowViewportOverflow: true });
+  });
+
   it('Settings screenshot must be identical', function () {
     return this.browser.url('/settings').assertView('Setting screen', 'body');
   });
