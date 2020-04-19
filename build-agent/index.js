a2e6235abed1f7f1c20845cef496919994ddd172
mgkOV <myagkov.mww@gmail.com>
2020-04-19 18:42:13 +0300

    add rimraf

--- build-agent/index.js
+++ build-agent/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { port } = require("./agent-conf");
+const axios = require("axios");
+const { port, serverHost, serverPort } = require("./agent-conf");
 
 require("express-async-errors");
 
@@ -9,6 +10,11 @@ app.use(express.json());
 
 require("./routes")(app);
 
-app.listen(port, () => {
-  console.log(`Build server run on port ${port}`);
+app.listen(port, async () => {
+  console.log(`Build agent run on port ${port}`);
+  try {
+    await axios.post(`http://${serverHost}:${serverPort}/notify-agent`, { port });
+  } catch (error) {
+    console.log(error.message, error.config);
+  }
 });
--- build-agent/routes.js
+++ build-agent/routes.js
@@ -1,3 +1,45 @@
+const rimraf = require("rimraf");
+const { promisify } = require("util");
+const { spawn } = require("child_process");
+const path = require("path");
+
+const githubOptions = {
+  headers: { Accept: "application/vnd.github.v3+json" }
+};
+
+const { ghToken } = require("./agent-conf");
+
+if (ghToken) {
+  githubOptions.headers["Authorization"] = `token ${ghToken}`;
+}
+
 module.exports = (app) => {
-  console.log("routes");
+  app.post("/build", async (req, res) => {
+    let { build } = req.body;
+    build = {
+      id: "b1d16840-3d22-48ee-af2b-4dd512504875",
+      commitHash: "c120f44d3d29c8e822a92e1d6879b8b77be6b9dc",
+      repoName: "axios/axios",
+      buildCommand: "npm run build"
+    };
+
+    await promisify(rimraf)(path.join(".", "storage"));
+
+    // клонируем репозиторий в storage
+    const repository = await axios.get(`https://api.github.com/repos/${build.repoName}`);
+
+    const child = spawn("git", ["clone", repository.clone_url, "storage"]);
+    console.info("Start cloning...");
+
+    child.on("exit", (code) => {
+      console.info("Finish cloning...");
+      if (code === 0) next();
+    });
+
+    child.on("error", (err) => {
+      console.log("Cloning error...");
+    });
+
+    console.log(build);
+  });
 };
