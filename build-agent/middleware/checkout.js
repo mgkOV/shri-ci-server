bf3eb97b7326343904fe45b908e2c62857f267ae
mgkOV <myagkov.mww@gmail.com>
2020-04-20 00:47:32 +0300

    build properly

--- build-agent/middleware/checkout.js
+++ build-agent/middleware/checkout.js
@@ -5,10 +5,9 @@ const axios = require("axios");
 module.exports = (req, res, next) => {
   let { build } = req.body;
   build = {
-    id: "b1d16840-3d22-48ee-af2b-4dd512504875",
-    commitHash: "c120f44d3d29c8e822a92e1d6879b8b77be6b9dc",
-    repoName: "axios/axios",
-    buildCommand: "cd storage && npm i && npm run test"
+    commitHash: "d5a66494928e2f24d4",
+    repoName: "mgkOV/shri-ci-test",
+    buildCommand: "npm i && npm run build"
   };
 
   // Делаем checkout нужного комита
--- build-agent/routes.js
+++ build-agent/routes.js
@@ -7,22 +7,23 @@ const checkout = require("./middleware/checkout");
 
 module.exports = (app) => {
   app.post("/build", clone, checkout, async (req, res) => {
+    let { build } = req.body;
+
     build = {
-      id: "b1d16840-3d22-48ee-af2b-4dd512504875",
-      commitHash: "c120f44d3d29c8e822a92e1d6879b8b77be6b9dc",
-      repoName: "axios/axios",
+      commitHash: "d5a66494928e2f24d4",
+      repoName: "mgkOV/shri-ci-test",
       buildCommand: "npm i && npm run build"
     };
 
     exec(build.buildCommand, { cwd: path.join(".", "storage") }, (err, stdout, stderr) => {
       if (err) {
         console.log("Error build...");
-        console.log(stderr);
+        console.log(stderr.toString());
         return;
       }
 
       console.log("Finish build...");
-      console.log(stdout);
+      console.log(stdout.toString());
     });
   });
 };
