92e3d63f35bc01eeefd574794d67bc6dbac0c3fd
mgkOV <myagkov.mww@gmail.com>
2020-04-19 16:37:21 +0300

    add build controller

--- build-server/controller.js
+++ build-server/controller.js
@@ -1,28 +1,51 @@
+const axios = require("axios");
 const api = require("./api");
 
 module.exports = async (agents) => {
-  const waitingBuilds = await getWaitingBuilds();
-
-  console.log(waitingBuilds);
-
-  setInterval(() => {
-    console.log(agents);
-  }, 10000);
+  for await (let i of loadAgents(agents)) {
+  }
 };
 
+async function* loadAgents(buildAgents, period = 7000) {
+  while (true) {
+    const waitingBuilds = await getWaitingBuilds();
+
+    try {
+      for (let prop in buildAgents) {
+        if (!buildAgents[prop].build) {
+          const build = waitingBuilds.pop();
+          const agent = buildAgents[prop];
+
+          agent.build = build;
+          const status = await axios.post(`http://${agent.host}:${agent.port}/build`, build);
+        }
+      }
+
+      await new Promise((resolve) => setTimeout(resolve, period));
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+}
+
 async function getWaitingBuilds() {
-  const settings = await api.getConfig();
-  if (!settings || !settings.data || !settings.data.id) return {};
+  try {
+    const settings = await api.getConfig();
+    if (!settings || !settings.data || !settings.data.id) return [];
 
-  let builds = await api.getBuildList(0, 100);
-  waitingBuilds = builds.data
-    .filter(({ status }) => status === "Waiting")
-    .map(({ id, commitHash }) => ({
-      id,
-      commitHash,
-      repoName: settings.data.repoName,
-      buildCommand: settings.data.buildCommand
-    }));
+    let builds = await api.getBuildList(0, 100);
+    waitingBuilds = builds.data
+      .filter(({ status }) => status === "Waiting")
+      .map(({ id, commitHash }) => ({
+        id,
+        commitHash,
+        repoName: settings.data.repoName,
+        buildCommand: settings.data.buildCommand
+      }));
 
-  return waitingBuilds;
+    return waitingBuilds;
+  } catch (error) {
+    console.log(error.message);
+    return [];
+  }
 }
