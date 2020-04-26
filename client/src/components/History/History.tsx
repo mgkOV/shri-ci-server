9271864be44d46f0f13e3132b6a6cde3b8a04f52
mgkOV <myagkov.mww@gmail.com>
2020-04-26 18:21:56 +0300

    convert History

---
+++ client/src/components/History/History.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { cn, ClassNameList } from "@bem-react/classname";
+
+import "./History.scss";
+
+type History = React.FC<{
+  mix: ClassNameList;
+}>;
+
+const History: History = ({ children, mix }) => {
+  const historyStyles = cn("History")(null, mix);
+
+  return <div className={historyStyles}>{children}</div>;
+};
+
+export default History;
