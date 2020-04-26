3d193fd5b155637faad86fe781943cddd6552ca1
mgkOV <myagkov.mww@gmail.com>
2020-04-26 18:18:20 +0300

    conver StartScreen, ErrorBoundry, BtnGroup

--- client/src/components/StartScreen/Icon/StartScreen-Icon.tsx
+++ client/src/components/StartScreen/Icon/StartScreen-Icon.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ReactComponent as StartIcon } from "./start-icon.svg";
 
-const StartScreenIcon = (props) => (StartIcon ? <StartIcon /> : null);
+const StartScreenIcon: React.FC = () => (StartIcon ? <StartIcon /> : null);
 
 export default StartScreenIcon;
---
+++ client/src/components/StartScreen/Title/StartScreen-Title.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./StartScreen-Title.scss";
+
+const StartScreenTitle: React.FC = ({ children }) => {
+  return <div className="StartScreen-Title">{children}</div>;
+};
+
+export default StartScreenTitle;
