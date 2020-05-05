61f48282d572f474e3515a1ae2d3b9bebf5b5cab
mgkOV <myagkov.mww@gmail.com>
2020-05-05 22:37:19 +0300

    change cache strategy to CACHE ONLY

--- client/build/sw.js
+++ client/build/sw.js
@@ -1,12 +1,22 @@
+importScripts("/precache-manifest.86023acacd0688bfb4b9773b22e4694c.js");
+const urlToCache = self.__precacheManifest.map((i) => i.url);
+
 const CACHE = "shri-ci-cache";
 
 // Кэшируем статику
 self.addEventListener("install", (event) => {
-  importScripts("/precache-manifest.86023acacd0688bfb4b9773b22e4694c.js");
-  const urlToCache = self.__precacheManifest.map((i) => i.url);
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+      .then(() => {
+        console.log("Устаревший кэш очищен...");
+        return caches.open(CACHE);
+      })
+      .then((cache) => cache.addAll(urlToCache))
+  );
 
-  console.log("Initial caching");
-  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(urlToCache)));
+  self.skipWaiting();
 });
 
 self.addEventListener("fetch", function (event) {
@@ -14,8 +24,6 @@ self.addEventListener("fetch", function (event) {
   if (!event.request.url.match(/\/static\//i)) return;
 
   event.respondWith(fromCache(event.request));
-  // Обновляем кэш
-  event.waitUntil(update(event.request));
 });
 
 function fromCache(request) {
@@ -24,10 +32,4 @@ function fromCache(request) {
     .then((cache) =>
       cache.match(request).then((matching) => matching || Promise.reject("no-match"))
     );
-}
-
-function update(request) {
-  return caches
-    .open(CACHE)
-    .then((cache) => fetch(request).then((response) => cache.put(request, response)));
 }
